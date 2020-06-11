b70c0fe3680dfcf80915d68f9443e31114094c08
Leonardo Rosa <leonardodarosa@unochapeco.edu.br>
2020-06-11 17:57:43 -0300

    Fixed: Bug when the Deserializer tries to parse the included relationships.

--- tests/mocks/deserializedObject.js
+++ tests/mocks/deserializedObject.js
@@ -8,7 +8,27 @@ export default {
     id: 1,
     logo: '/assets/fallback/my-logo.png',
     config_links: {},
-    config_meta: {}
+    config_meta: {},
+    benefits: [
+      {
+        id: 1,
+        value_type: 'Percentage',
+        periodicity: 'Monthly',
+        extensible: true,
+        value: 80.00,
+        benefit_links: {},
+        benefit_meta: {},
+      },
+      {
+        id: 2,
+        value_type: 'Value',
+        periodicity: 'Monthly',
+        extensible: false,
+        value: 299.90,
+        benefit_links: {},
+        benefit_meta: {},
+      },
+    ]
   },
   addresses: [
     {
